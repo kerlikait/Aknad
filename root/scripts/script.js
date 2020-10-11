e4d840878a08f0d5fcf4bf996f42934985f4524f
kerlikait <kerli.ounamaa@gmail.com>
2020-10-11 17:36:27 +0300

    Added videos

--- root/scripts/script.js
+++ root/scripts/script.js
@@ -9,15 +9,25 @@ function info(obj) {
 
   if ( obj.id  == "minimeerimisNupp") {
     changeBorder(obj);
-    document.getElementById("infoBox").innerHTML = '<img src="./images/Slaid2.JPG"  id="infoBoxImage">';
+    document.getElementById("infoBox").innerHTML = '<img src="./images/Minimize.png"  id="infoBoxImage">' +
+    '<video width="900" controls>  <source src="./images/Minimize.mp4" type="video/mp4">  Your browser does not support the video tag. </video>' +
+    '<img src="./images/MinimizeAll.png"  id="infoBoxImage">' +
+    '<video width="900" controls>  <source src="./images/Desktop.mp4" type="video/mp4">  Your browser does not support the video tag. </video>' +
+    '<img src="./images/RaputusText.png"  id="infoBoxImage">' +
+    '<video width="900" controls>  <source src="./images/Shake.mp4" type="video/mp4">  Your browser does not support the video tag. </video>';
   }
   else if (obj.id  == "suurendamis/vähendamisNupp") {
     changeBorder(obj);
-    document.getElementById("infoBox").innerHTML = '<img src="./images/Slaid3.JPG"  id="infoBoxImage"> <img width="500" src="./images/WinKeys.JPG"  id="infoBoxImage"> <video width="900" controls>  <source src="./images/WindowPosition.mp4" type="video/mp4">  Your browser does not support the video tag. </video>';
+    document.getElementById("infoBox").innerHTML = '<img src="./images/KeskmineNupp.png"  id="infoBoxImage"> ' +
+    '<img width="600" src="./images/SmallWindow.png"  id="infoBoxImage">' +
+    '<video width="900" controls>  <source src="./images/Resize.mp4" type="video/mp4">  Your browser does not support the video tag. </video>' +
+    '<img width="500" src="./images/WinKeys.JPG"  id="infoBoxImage">' +
+    '<video width="900" controls>  <source src="./images/WindowPosition.mp4" type="video/mp4">  Your browser does not support the video tag. </video>';
   }
   else if (obj.id  == "sulgemisNupp") {
     changeBorder(obj);
-    document.getElementById("infoBox").innerHTML = '<img src="./images/Slaid4.JPG"  id="infoBoxImage">';
+    document.getElementById("infoBox").innerHTML = '<img src="./images/Close.png"  id="infoBoxImage">' +
+    '<video width="900" controls>  <source src="./images/Close.mp4" type="video/mp4">  Your browser does not support the video tag. </video>';
   }
 }
 
